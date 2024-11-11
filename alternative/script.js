14c01e27a7338ee9311c3b1c110b81a0b6f8196b
robertsalzwedel <robert.salzwedel@posteo.de>
2024-11-11 15:18:30 +0100

    added figure

---
+++ alternative/script.js
@@ -0,0 +1,5 @@
+// No longer necessary for the current structure, but can be added later if needed
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
